48b08108c85223e419e1b9a12d7a4b7b88b5a849
SowmyaGonepally <sowmya.gonepally@zemosolabs.com>
2021-12-07 13:43:27 +0530

    lombok added to UserRegistrationDto

--- library-system/src/main/java/com/sowmya/librarysystem/dto/UserRegistrationDto.java
+++ library-system/src/main/java/com/sowmya/librarysystem/dto/UserRegistrationDto.java
@@ -1,8 +1,11 @@
 package com.sowmya.librarysystem.dto;
 
+import lombok.Data;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Data
 public class UserRegistrationDto {
 
     @NotNull(message = "is required")
@@ -31,35 +34,4 @@ public class UserRegistrationDto {
         this.password = password;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
