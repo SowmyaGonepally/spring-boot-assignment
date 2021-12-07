f921d183e4626991d54eb2cfe51a4b58970d4f0d
SowmyaGonepally <sowmya.gonepally@zemosolabs.com>
2021-12-07 12:37:03 +0530

    CustomerController HTTP method changed

--- library-system/src/main/java/com/sowmya/librarysystem/controller/CustomErrorController.java
+++ library-system/src/main/java/com/sowmya/librarysystem/controller/CustomErrorController.java
@@ -4,14 +4,14 @@ package com.sowmya.librarysystem.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
-    @RequestMapping("/error")
+    @GetMapping("/error")
     public String handleError(HttpServletResponse response)
     {
         Object status = response.getStatus();
